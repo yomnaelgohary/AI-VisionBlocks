7bec55025fbcd1885efd2299779c83b3af4e22d7
yomnaelgohary <yomnaelgohary011@gmail.com>
2026-05-08 09:03:45 +0300

    .

--- apps/api/app/main.py
+++ apps/api/app/main.py
@@ -22,7 +22,7 @@ app = FastAPI(title=settings.API_TITLE, version=settings.API_VERSION)
 # CORS (allow local web app on 3000)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://aivisionblocks-cncdg0ms6-yomnas-projects-0d98906c.vercel.app/"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://ai-5othaizsb-yomnas-projects-0d98906c.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
